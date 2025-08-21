4219be3488b5b018b1481e04fadf8c30904a05d8
Pablo Barcellos Soares <74993616+PabloBarcellos-0522@users.noreply.github.com>
2025-08-21 15:07:37 -0300

    Exercise update

--- Aula18082025/Aula18082025/Program.cs
+++ Aula18082025/Aula18082025/Program.cs
@@ -6,7 +6,7 @@ namespace Aula18082025
     {
         static void Main(string[] args)
         {
-            //Exercício Aeroporto
+            // Exercício Aeroporto
 
             Airport a1 = new();
             Airport a2 = new();
@@ -22,20 +22,120 @@ namespace Aula18082025
 
             ft1.Fly(a1, 100);
             a1.Transportes[0].EuSou();
+            Console.WriteLine("\n\n\n");
 
 
-            //Gestão de pessoas IBGE
-
-            int idade;
+            // Gestão de pessoas IBGE
+            string idade;
             List<int> idades = [];
-
             do
             {
-                idade = int.Parse(Console.ReadLine());
-                if (idade > 0)
-                    idades.Add(idade);
+                idade = Console.ReadLine() ?? string.Empty;
+                if (int.Parse(idade) > 0)
+                    idades.Add(int.Parse(idade));
             }
-            while (idade > 0);
+            while (int.Parse(idade) > 0 || idade == "");
+
+            Console.WriteLine($"Faixa A: {Math.Round(IdadeAgrupada(1, 15, idades))}");
+            Console.WriteLine($"Faixa B: {Math.Round(IdadeAgrupada(16, 30, idades))}");
+            Console.WriteLine($"Faixa C: {Math.Round(IdadeAgrupada(31, 45, idades))}");
+            Console.WriteLine($"Faixa D: {Math.Round(IdadeAgrupada(46, 60, idades))}");
+            Console.WriteLine($"Faixa E: {Math.Round(IdadeAgrupada(61, 999, idades))}\n\n");
+
+
+
+            // Verão Capixaba 01
+
+            Console.WriteLine("Escolha a conversão:");
+            Console.WriteLine("1 - Celsius para Fahrenheit");
+            Console.WriteLine("2 - Fahrenheit para Celsius");
+            Console.WriteLine("3 - Celsius para Kelvin");
+            Console.WriteLine("4 - Kelvin para Celsius");
+
+            Console.Write("Opção: ");
+            int opcao = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            Console.Write("Digite a temperatura: ");
+            double temp = double.Parse(Console.ReadLine() ?? string.Empty);
+
+            Console.WriteLine(ConverterTemp(opcao, temp));
+
+            Console.WriteLine("\n\n");
+
+
+
+            // Verão Capixaba - Média de Temperaturas
+            const int DIAS = 15;
+            double soma = 0;
+            double temperatura;
+
+            for (int i = 1; i <= DIAS; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Digite a temperatura do dia {i}: ");
+                    temperatura = double.Parse(Console.ReadLine() ?? string.Empty);
+
+                    if (temperatura > 28)
+                    {
+                        soma += temperatura;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Temperatura inválida! No verão capixaba deve ser acima de 28 °C.");
+                    }
+                }
+            }
+
+            double media = soma / DIAS;
+            Console.WriteLine($"\nA temperatura média no verão capixaba foi: {Math.Round(media)} °C");
+
+
+
+        }
+
+        public static double IdadeAgrupada(int idadeI, int idadeF, List<int> lista)
+        {
+            double quantidade = 0;
+            foreach (var item in lista)
+            {
+                if (item >= idadeI && item <= idadeF)
+                {
+                    quantidade++;
+                }
+            }
+            return (quantidade / lista.Count) * 100;
+        }
+
+        public static string ConverterTemp(int opcao, double temp)
+        {
+            double resultado = 0;
+
+            switch (opcao)
+            {
+                case 1:
+                    // Celsius para Fahrenheit
+                    resultado = (temp * 9 / 5) + 32;
+                    return ($"{temp} °C = {resultado} °F");
+                case 2:
+                    // Fahrenheit para Celsius
+                    resultado = (temp - 32) * 5 / 9;
+                    return ($"{temp} °F = {resultado} °C");
+
+                case 3:
+                    // Celsius para Kelvin
+                    resultado = temp + 273.15;
+                    return ($"{temp} °C = {resultado} K");
+
+                case 4:
+                    // Kelvin para Celsius
+                    resultado = temp - 273.15;
+                    return ($"{temp} K = {resultado} °C");
+
+                default:
+                    return ("Opção inválida!");
+            }
         }
     }
 }
