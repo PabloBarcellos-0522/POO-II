dce12be32b310dbb9e9fb46aecaa0ec6ba9a2a83
Pablo <pablobarcellossoares@gmail.com>
2025-08-25 16:06:17 -0300

    Edit

---
+++ Aula25082025/ConversorTemp/ConversorTemp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConversorTemp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double tempC;
+        public double tempF;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                tempC = double.Parse(InputTMP.Text);
+                tempF = tempC * 1.8 + 32;
+
+
+                OutputTMP.Content = tempF + "º Fahrenheit";
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex);
+            }
+
+
+        }
+
+        private void InputTMP_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            try
+            {
+                tempC = double.Parse(InputTMP.Text);
+                tempF = tempC * 1.8 + 32;
+
+
+                OutputTMP.Content = tempF + "º Fahrenheit";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+        }
+    }
+}
